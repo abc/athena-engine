0cc153f143f102589839c4222e0bdab8780b79c6
chartax <chartax@gmail.com>
2014-02-19 12:12:15 +0000

    Added collision interface.

---
+++ VS/Athena/Athena/Interfaces/ICanCollide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Athena.Classes
+{
+    interface ICanCollide <T>
+    {
+        bool CollidesWith (T compareWith);
+
+        BoundingBox Bounds
+        {
+            get;
+            set;
+        }
+    }
+}
