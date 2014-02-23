f0d71c0a17ad8bb21ea2cbd18948723bc3178bd8
Alastair Campbell <alastair.campbell@outlook.com>
2014-02-23 16:58:36 +0000

    Added instructions/files to install XNA 4.0

--- VS/Athena/Athena/Athena/Game1.cs
+++ VS/Athena/Athena/Athena/Game1.cs
@@ -21,6 +21,7 @@ namespace Athena
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         BatchHandler batchHandler;
+        Character Player;
 
         public Game1()
         {
@@ -48,13 +49,31 @@ namespace Athena
         /// 
 
         Texture2D Blank;
+        char[,] tiles = new char[320, 240];
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             batchHandler = new BatchHandler(spriteBatch);
             Blank = this.Content.Load<Texture2D>("blank");
+            Player = new Character(5, 5, Blank); 
 
+            int counter = 0;
+            string line;
+
+            System.IO.StreamReader file =
+                new System.IO.StreamReader("testlevel.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                char[] chars = line.ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                        tiles[i, counter] = chars[i];
+                }
+                    counter++;
+            }
             // TODO: use this.Content to load your game content here
         }
 
@@ -73,11 +92,34 @@ namespace Athena
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         /// 
+        KeyboardState oldState;
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+
+
+            var newState = Keyboard.GetState();
+
+            if (newState.IsKeyDown(Keys.W) && !oldState.IsKeyDown(Keys.W))
+            {
+                Player.MoveUp();
+            }
+            if (newState.IsKeyDown(Keys.A) && !oldState.IsKeyDown(Keys.A))
+            {
+                Player.MoveLeft();
+            }
+            if (newState.IsKeyDown(Keys.S) && !oldState.IsKeyDown(Keys.S))
+            {
+                Player.MoveDown();
+            }
+            if (newState.IsKeyDown(Keys.D) && !oldState.IsKeyDown(Keys.D))
+            {
+                Player.MoveRight();
+            }
+
+            oldState = newState;
 
             // TODO: Add your update logic here
 
@@ -93,39 +135,52 @@ namespace Athena
         
         protected override void Draw(GameTime gameTime)
         {
+
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            batchHandler.Draw<Tile>(new Tile(2, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(2, 3, Blank));
-            batchHandler.Draw<Tile>(new Tile(2, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(2, 5, Blank));
-            batchHandler.Draw<Tile>(new Tile(2, 6, Blank));
-            batchHandler.Draw<Tile>(new Tile(3, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(3, 6, Blank));
-            batchHandler.Draw<Tile>(new Tile(5, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(5, 3, Blank));
-            batchHandler.Draw<Tile>(new Tile(5, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(5, 5, Blank));
-            batchHandler.Draw<Tile>(new Tile(5, 6, Blank));
-            batchHandler.Draw<Tile>(new Tile(6, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(7, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(7, 3, Blank));
-            batchHandler.Draw<Tile>(new Tile(7, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(7, 5, Blank));
-            batchHandler.Draw<Tile>(new Tile(7, 6, Blank));
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    if (tiles[i, j] == 'X')
+                    {
+                        batchHandler.Draw<Tile>(new Tile(i, j, Blank));
+                    }
+                }
+            }
+            //batchHandler.Draw<Tile>(new Tile(2, 3, Blank));
+            //batchHandler.Draw<Tile>(new Tile(2, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(2, 5, Blank));
+            //batchHandler.Draw<Tile>(new Tile(2, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(3, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(3, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(5, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(5, 3, Blank));
+            //batchHandler.Draw<Tile>(new Tile(5, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(5, 5, Blank));
+            //batchHandler.Draw<Tile>(new Tile(5, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(6, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(7, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(7, 3, Blank));
+            //batchHandler.Draw<Tile>(new Tile(7, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(7, 5, Blank));
+            //batchHandler.Draw<Tile>(new Tile(7, 6, Blank));
 
-            batchHandler.Draw<Tile>(new Tile(9, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(10, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(9, 3, Blank));
-            batchHandler.Draw<Tile>(new Tile(9, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(9, 5, Blank));
-            batchHandler.Draw<Tile>(new Tile(9, 6, Blank));
-            batchHandler.Draw<Tile>(new Tile(10, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(11, 2, Blank));
-            batchHandler.Draw<Tile>(new Tile(11, 3, Blank));
-            batchHandler.Draw<Tile>(new Tile(11, 4, Blank));
-            batchHandler.Draw<Tile>(new Tile(11, 5, Blank));
-            batchHandler.Draw<Tile>(new Tile(11, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(9, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(10, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(9, 3, Blank));
+            //batchHandler.Draw<Tile>(new Tile(9, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(9, 5, Blank));
+            //batchHandler.Draw<Tile>(new Tile(9, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(10, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(11, 2, Blank));
+            //batchHandler.Draw<Tile>(new Tile(11, 3, Blank));
+            //batchHandler.Draw<Tile>(new Tile(11, 4, Blank));
+            //batchHandler.Draw<Tile>(new Tile(11, 5, Blank));
+            //batchHandler.Draw<Tile>(new Tile(11, 6, Blank));
+            //batchHandler.Draw<Tile>(new Tile(15, 15, Blank));
+            batchHandler.Draw<Character>(Player);
             spriteBatch.End();
             // TODO: Add your drawing code here
 
