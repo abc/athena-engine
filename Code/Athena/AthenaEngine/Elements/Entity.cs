54f4e6d99aec2cddcc17c080f7a2e3077c41f94f
Alastair Campbell <alastair@acampbell.co>
2014-02-28 16:57:53 +0000

    Setup MonoGame project

---
+++ Code/Athena/AthenaEngine/Elements/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AthenaEngine.Elements
+{
+    class Entity : Interfaces.IDrawable
+    {
+        public Entity()
+        {
+
+        }
+    }
+}
---
+++ Code/Athena/AthenaEngine/Interfaces/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AthenaEngine.Interfaces
+{
+    interface IDrawable
+    {
+        public void Draw();
+    }
+}
